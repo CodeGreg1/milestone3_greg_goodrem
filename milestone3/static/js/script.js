792b2a9bdf33be6cdda96ca37e8664343759d9f6
GregWebDev <code@greggoodrem.co.uk>
2022-10-27 09:47:32 +0000

    Resited JS module so no conflicts

--- milestone3/static/js/script.js
+++ milestone3/static/js/script.js
@@ -16,39 +16,18 @@ document.addEventListener("DOMContentLoaded", function() {
     M.Collapsible.init(collapsibles);
 });
 
-const COGS = document.querySelectorAll('.fa-cog')
-const BOTTOM_CARD = document.querySelectorAll('.card-action.cog')
+// const COGS = document.querySelectorAll('.fa-cog')
+// const BOTTOM_CARD = document.querySelectorAll('.card-action.cog')
 
 
-COGS.forEach((cog)=>{
-    cog.addEventListener('click', function() {
-        console.log('Cog clicked')
-        BOTTOM_CARD.forEach((bot)=>{
-            bot.classList.toggle('vis')})})})
-        //   May need to be in own script page
-const FULLNAME = document.getElementById("fullname")
+// COGS.forEach((cog)=>{
+//     cog.addEventListener('click', function() {
+//         console.log('Cog clicked')
+//         BOTTOM_CARD.forEach((bot)=>{
+//             bot.classList.toggle('vis')})})})
 
-FULLNAME.addEventListener('change',function(e){
-  let opttext=FULLNAME.options[FULLNAME.options.selectedIndex].text;
-  console.log(opttext);
-  let nameArr = opttext.split("-")
-
-  let tCliName = nameArr[0].trim()
-  let tCliUsr = nameArr[1].trim().slice(1,-1)
-
-  console.log(tCliName);
-  console.log(tCliUsr);
-
-  treatment_client.value = tCliName
-  tusername.value = tCliUsr
-})
-
-// FULLNAME.addEventListener('change',function(e){
-//   let opttext=FULLNAME.options[FULLNAME.options.selectedIndex].text;
-//   console.log(opttext);
-//   treatment_client.value = opttext
-let bookingInfo = document.querySelector(".J6OThpvEmEUa1wPmQ9tL.XZiDpbDeP43TyKu02OUE")
-let upcoming = document.querySelector(".upcoming-booking")
+// let bookingInfo = document.querySelector(".J6OThpvEmEUa1wPmQ9tL.XZiDpbDeP43TyKu02OUE")
+// let upcoming = document.querySelector(".upcoming-booking")
 
 
 // This is the text with the confirmed Appointment date /html/body/div[1]/div/div/div/div/div/div[2]/div/div[2]/text()
